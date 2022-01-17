b43b559134d5b9026cefb378ea5c54bae3ee2c92
funkyblues <dnr8874@naver.com>
2022-01-17 21:29:45 +0900

    8-9th assignment--fin.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-const inputNumber = document.querySelector(".inputNumber");
+const inputNumber = document.querySelector(".inputNumber--get");
+let userNumberGet = document.querySelector(".userNumber--get");
+let userNumberPost = document.querySelector(".userNumber--post");
+let guessTheNumber = document.querySelector(".inputNumber--post");
+const playGame = document.querySelector(".play__game");
+const hidden = document.querySelector(".hidden");
+const gameResult = document.querySelector(".gameResult");
 
-function getInputNumber(event) {
-  event.preventDefault();
-  console.log(inputNumber.value);
-};
+function letsPlayGame() {
+  hidden.classList.remove("hidden");
+  guessTheNumber.innerText = Math.ceil(inputNumber.value * Math.random()); 
+  userNumberPost.innerText = userNumberGet.value;
+  if (guessTheNumber.innerText === userNumberPost.innerText) {
+    gameResult.innerText = "You won!";
+  }
+  else {
+    gameResult.innerText = "You lost!";
+  }
+}
 
-inputNumber.addEventListener("click", getInputNumber);
+
+playGame.addEventListener("click", letsPlayGame);
