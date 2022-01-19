216c7a928a4dcc27c7ee67d2b07c7b1887733646
funkyblues <dnr8874@naver.com>
2022-01-19 17:38:30 +0900

    10th-assingmnet fin.

--- src/index.js
+++ src/index.js
@@ -7,19 +7,26 @@ function countClock() {
   let diffInTotalTime = xDay.getTime() - today.getTime(); // 이거는 ms단위. 총 시간
   let DIVIDE_CONSTANT = 1000 * 60 * 60 * 24;
   let diffDays = Math.floor(diffInTotalTime / DIVIDE_CONSTANT);
-  let diffHours = Math.floor(
-    (diffInTotalTime / DIVIDE_CONSTANT - diffDays) * 24
-  );
-  let diffMinutes =
+  let diffHours = String(
+    Math.floor((diffInTotalTime / DIVIDE_CONSTANT - diffDays) * 24)
+  ).padStart(2, "0");
+  let diffMinutes = String(
     Math.floor((diffInTotalTime / DIVIDE_CONSTANT - diffDays) * 24 * 60) -
-    diffHours * 60;
-  let diffSeconds = Math.floor(
-    (diffInTotalTime / DIVIDE_CONSTANT - diffDays) * 24 * 60 * 60 -
-      diffHours * 60 * 60 -
-      diffMinutes * 60
-  );
-  clockTitle.innerText = `${diffDays}d ${diffHours}h ${diffMinutes}m ${diffSeconds}s`;
+      diffHours * 60
+  ).padStart(2, "0");
+  let diffSeconds = String(
+    Math.floor(
+      (diffInTotalTime / DIVIDE_CONSTANT - diffDays) * 24 * 60 * 60 -
+        diffHours * 60 * 60 -
+        diffMinutes * 60
+    )
+  ).padStart(2, "0");
+  clockTitle.innerText =
+    `${diffDays}d ${diffHours}h ${diffMinutes}m ${diffSeconds}s`.padStart(
+      2,
+      "0"
+    );
 }
-
+// 토큰 뭐임?
 countClock();
 setInterval(countClock, 1000);
