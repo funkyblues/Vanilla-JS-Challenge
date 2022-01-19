5a5b3c3eee090ed9e14b4e24f528a93522099475
funkyblues <dnr8874@naver.com>
2022-01-19 17:24:44 +0900

    10th-assignment(시간 두자리로 맞춰야 함)

--- src/index.js
+++ src/index.js
@@ -4,29 +4,21 @@ function countClock() {
   let xDay = new Date(2022, 11, 25, 0, 0, 0);
   let today = new Date();
 
-  const todayYear = today.getFullYear();
-  const todayMonth = today.getMonth();
-  const todayDate = today.getDate();
-  const todayHours = today.getHours();
-  const todayMinutes = today.getMinutes();
-  const todaySeconds = today.getSeconds();
-
-  const xYear = xDay.getFullYear();
-  const xMonth = xDay.getMonth();
-  const xDate = xDay.getDate();
-  const xHours = xDay.getHours();
-  const xMinutes = xDay.getMinutes();
-  const xSeconds = xDay.getSeconds();
-
-  console.log(
-    todayYear,
-    todayMonth,
-    todayDate,
-    todayHours,
-    todayMinutes,
-    todaySeconds
+  let diffInTotalTime = xDay.getTime() - today.getTime(); // 이거는 ms단위. 총 시간
+  let DIVIDE_CONSTANT = 1000 * 60 * 60 * 24;
+  let diffDays = Math.floor(diffInTotalTime / DIVIDE_CONSTANT);
+  let diffHours = Math.floor(
+    (diffInTotalTime / DIVIDE_CONSTANT - diffDays) * 24
   );
-  console.log(xYear, xMonth, xDate, xHours, xMinutes, xSeconds);
+  let diffMinutes =
+    Math.floor((diffInTotalTime / DIVIDE_CONSTANT - diffDays) * 24 * 60) -
+    diffHours * 60;
+  let diffSeconds = Math.floor(
+    (diffInTotalTime / DIVIDE_CONSTANT - diffDays) * 24 * 60 * 60 -
+      diffHours * 60 * 60 -
+      diffMinutes * 60
+  );
+  clockTitle.innerText = `${diffDays}d ${diffHours}h ${diffMinutes}m ${diffSeconds}s`;
 }
 
 countClock();
